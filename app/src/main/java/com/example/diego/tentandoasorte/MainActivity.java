aacf8d98fe9da3e670120e8721c7b63104816cdd
diegom3nd35 <diegom3nd35@gmail.com>
2018-11-28 11:46:19 -0300

    chamando o metodo sortear no onClick

--- app/src/main/java/com/example/diego/tentandoasorte/MainActivity.java
+++ app/src/main/java/com/example/diego/tentandoasorte/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.diego.tentandoasorte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sortearNumero (){
+    public void sortearNumero (View view){
+
+        TextView textResuldado = findViewById(R.id.textResultado);
+        textResuldado.setText("O sorteado foi: ");
 
     }
 }

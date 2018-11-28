e3f5b01f57830019061eec7695a8d6c92975e3de
diegom3nd35 <diegom3nd35@gmail.com>
2018-11-28 11:23:14 -0300

    Adicionando tela sortear

---
+++ app/src/main/java/com/example/diego/tentandoasorte/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.diego.tentandoasorte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero (){
+
+    }
+}
